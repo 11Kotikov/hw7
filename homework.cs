6fe4a9d9aa8d352f7ebc71b58179e171194c5721
Kotikov <11deadsluts@gmail.com>
2022-10-16 02:43:03 +0300

    task4 is solved

--- homework.cs
+++ homework.cs
@@ -1,16 +1,11 @@
 ﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-Console.WriteLine("Input number: ");
+Console.WriteLine("Input an even number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ($"You typed {num1}");
-
+int current = 2;
 while (current<=num1)
 
-    if (num1%2==0)
-{
-    Console.WriteLine ($"{num1} is even!");
-}
-else 
-{
-    Console.WriteLine ("Plese, try again, this is odd");
-
-}
+    {
+    Console.Write ($"{current} ");
+    current=current+2;
+    }

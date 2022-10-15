4caec031af677cf245bbc3cda6fcffc48096a1bb
Kotikov <11deadsluts@gmail.com>
2022-10-16 02:04:30 +0300

    start task4

--- homework.cs
+++ homework.cs
@@ -1,15 +1,1 @@
-﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-Console.WriteLine("Input number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ($"You typed {num1}");
-
-
-if (num1%2==0)
-{
-    Console.WriteLine ($"{num1} is even!");
-}
-else 
-{
-    Console.WriteLine ("Plese, try again, this is odd");
-
-}
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

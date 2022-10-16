2c9db1424e6f380736281efe4dc75aee47ad88b3
Kotikov <11deadsluts@gmail.com>
2022-10-16 03:32:53 +0300

    limitless cycle task4.a

--- homework.cs
+++ homework.cs
@@ -1,4 +1,7 @@
 ﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//эксперементальная ветка
+
 Console.WriteLine("Input number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ($"You typed {num1}");

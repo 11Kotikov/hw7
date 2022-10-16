4ba742ca3a070fd553164484bf116b7670985c75
Kotikov <11deadsluts@gmail.com>
2022-10-16 03:37:15 +0300

    new alternative one task4.b

--- homework.cs
+++ homework.cs
@@ -1,4 +1,5 @@
 ﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/* 
 Console.WriteLine("Input an even number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ($"You typed {num1}");
@@ -9,5 +10,34 @@ while (current<=num1)
     Console.Write ($"{current} ");
     current=current+2;
     }
+    
+   //эта была последняя 
+   */
 
-    //эта была последняя
+   Console.WriteLine("Input a number bigger than 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+ Console.WriteLine ($"You typed {num1}");
+
+ if (num1>=current)
+ {
+   while (current<=num1)
+{
+       {
+        Console.Write ($"{current} ");
+        current=current+2;
+    }
+} 
+ }
+
+ else 
+  {
+    Console.WriteLine("Please try again and input a number bigger than 1: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    while (current<=num1)
+       {
+        Console.Write ($"{current} ");
+        current=current+2;
+    }
+  }
+
